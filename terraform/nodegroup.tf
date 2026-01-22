70261ba5220601bc0cdf0044063c2316209f49ef
rihad-a <Rihadwork@hotmail.com>
2026-01-22 03:01:14 +0000

    updating instance type to t3.medium

--- terraform/nodegroup.tf
+++ terraform/nodegroup.tf
@@ -2,7 +2,7 @@ resource "aws_eks_node_group" "eks-node-group" {
   cluster_name    = aws_eks_cluster.eks_labs.name
   node_group_name = "eks-node-group"
   node_role_arn   = aws_iam_role.eks-ng-role.arn
-  instance_types  = ["t3.small"]
+  instance_types  = ["t3.medium"]
   capacity_type   = "SPOT"
   subnet_ids = [
     aws_subnet.private_1.id,
